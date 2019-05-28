4a75d2ad2469eb9bf9390a0365065a36bbc5cdc6
Dmitry Groisman <dgroisma@redhat.com>
2019-05-28 23:38:33 +0300

    subm on kind

---
+++ tools/kind-subm/README.md
@@ -0,0 +1,69 @@
+# Create Kubernetes clusters with kind and submariner for e2e testing.
+
+## Prerequisites.
+
+- [go](https://golang.org/doc/install#install)
+- [kind](https://github.com/kubernetes-sigs/kind#installation-and-usage)
+- [kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+- [helm](https://helm.sh/docs/using_helm/#installing-helm)
+- [docker](https://docs.docker.com/install/)
+
+### Run the script
+
+```bash
+chmod a+x e2e.sh
+./e2e.sh
+```
+
+The script will install:
+
+- Three k8s clusters. Please see the cluster{1..3}-config.yaml for network configurations.
+  Cluster1: One master node for broker.
+  Cluster{2..3}: One master node and two worker nodes.
+  Te configuration can be changed in ***cluster{1..3}-config.yaml***.
+  Each node will reside in a docker container.
+- Helm on all clusters.
+- Submariner broker on cluster1 and gateways on clusters{2..3} nodes.
+- netshoot pod for testing on cluster2 and nginx deployment on cluster3.
+
+### Export kind kube config locations.
+
+```bash
+export KUBECONFIG=$HOME/.kube/kind-config-cluster1:$HOME/.kube/kind-config-cluster2:$HOME/.kube/kind-config-cluster3
+```
+
+Test with:
+
+```bash
+kubectl config get-contexts
+```
+
+You should see all three contexts for clusters{1..3} available.
+
+
+### Testing
+
+Get nginx service clusterIP from cluster3:
+
+```bash
+kubectl config use-context cluster3
+kubectl get svc -l app=nginx-demo
+```
+
+Switch context to cluster2 and curl from pod to a service on cluster3:
+
+```bash
+kubectl config use-context cluster2
+kubectl exec -it netshoot -- curl -I $nginx_svc_ip_cluster3
+```
+
+Please note that currently only pods to pods or pods to services connectivity is working.
+If you pass in ***e2e.sh*** cluster's (podSubnet) pods CIDR as serviceCIDR to submariner helm config, pods to pods connectivity will work.
+If you pass in ***e2e.sh*** cluster's (serviceSubnet) service CIDR as serviceCIDR to submariner helm config, pods to service connectivity will work.
+
+
+### Cleanup
+
+```bash
+for i in {1..3}; do kind delete cluster --name=cluster$i; done
+```
---
+++ tools/kind-subm/e2e.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+SUBMARINER_BROKER_NS=submariner-k8s-broker
+SUBMARINER_PSK=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+
+function kind_clusters() {
+    for i in {1..3}; do
+        kind create cluster --name=cluster$i --wait=5m --config=cluster$i-config.yaml
+        sed -i -- "s/user: kubernetes-admin/user: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
+        sed -i -- "s/name: kubernetes-admin.*/name: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
+    done
+
+    export KUBECONFIG=$HOME/.kube/kind-config-cluster1:$HOME/.kube/kind-config-cluster2:$HOME/.kube/kind-config-cluster3
+}
+
+function install_helm() {
+    for i in {1..3}; do
+        kubectl config use-context cluster$i
+        kubectl -n kube-system create serviceaccount tiller
+        kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+        helm init --service-account tiller
+        kubectl -n kube-system  rollout status deploy/tiller-deploy
+    done
+}
+
+function setup_gateway_nodes() {
+    for i in {1..3}; do
+        kubectl config use-context cluster$i
+        if [ $i -eq 1 ]; then
+            helm install submariner-latest/submariner-k8s-broker \
+                --name ${SUBMARINER_BROKER_NS} \
+                --namespace ${SUBMARINER_BROKER_NS}
+
+            SUBMARINER_BROKER_URL=$(kubectl -n default get endpoints kubernetes -o jsonpath="{.subsets[0].addresses[0].ip}:{.subsets[0].ports[?(@.name=='https')].port}")
+            SUBMARINER_BROKER_CA=$(kubectl -n ${SUBMARINER_BROKER_NS} get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='${SUBMARINER_BROKER_NS}-client')].data['ca\.crt']}")
+            SUBMARINER_BROKER_TOKEN=$(kubectl -n ${SUBMARINER_BROKER_NS} get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='${SUBMARINER_BROKER_NS}-client')].data.token}"|base64 --decode)
+        fi
+        if [ $i -eq 2 ]; then
+            worker_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster2-worker | head -n 1)
+            kubectl label node cluster$i-worker "submariner.io/gateway=true" --overwrite
+            helm install submariner-latest/submariner \
+                --name submariner \
+                --namespace submariner \
+                --set ipsec.psk="${SUBMARINER_PSK}" \
+                --set broker.server="${SUBMARINER_BROKER_URL}" \
+                --set broker.token="${SUBMARINER_BROKER_TOKEN}" \
+                --set broker.namespace="${SUBMARINER_BROKER_NS}" \
+                --set broker.ca="${SUBMARINER_BROKER_CA}" \
+                --set submariner.clusterId="cluster$i" \
+                --set submariner.clusterCidr="$worker_ip/32" \
+                --set submariner.serviceCidr="100.95.0.0/16" \
+                --set submariner.natEnabled="false"
+            echo Installing netshoot container on cluster$i worker: $worker_ip
+            cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: netshoot
+  namespace: default
+spec:
+  containers:
+    - name: netshoot
+      image: nicolaka/netshoot
+      imagePullPolicy: IfNotPresent
+      command:
+        - sleep
+        - "3600"
+  restartPolicy: Always
+EOF
+    else
+        worker_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster$i-worker | head -n 1)
+        kubectl label node cluster$i-worker "submariner.io/gateway=true" --overwrite
+        helm install submariner-latest/submariner \
+                --name submariner \
+                --namespace submariner \
+                --set ipsec.psk="${SUBMARINER_PSK}" \
+                --set broker.server="${SUBMARINER_BROKER_URL}" \
+                --set broker.token="${SUBMARINER_BROKER_TOKEN}" \
+                --set broker.namespace="${SUBMARINER_BROKER_NS}" \
+                --set broker.ca="${SUBMARINER_BROKER_CA}" \
+                --set submariner.clusterId="cluster$i" \
+                --set submariner.clusterCidr="$worker_ip/32" \
+                --set submariner.serviceCidr="100.96.0.0/16" \
+                --set submariner.natEnabled="false"
+        echo Installing nginx container on cluster$i worker: $worker_ip
+        cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-demo
+spec:
+  selector:
+    matchLabels:
+      app: nginx-demo
+  replicas: 2
+  template:
+    metadata:
+      labels:
+        app: nginx-demo
+    spec:
+      containers:
+        - name: nginx-demo
+          image: nginx:alpine
+          ports:
+            - containerPort: 80
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx-demo
+  labels:
+    app: nginx-demo
+spec:
+  type: ClusterIP
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 80
+  selector:
+    app: nginx-demo
+EOF
+    fi
+    done
+}
+
+helm repo add submariner-latest https://releases.rancher.com/submariner-charts/latest
+helm repo update
+
+kind_clusters
+install_helm
+setup_gateway_nodes
